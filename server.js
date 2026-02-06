7862963d0bf695a779ebaee5a2fd10a75a748d2b
Fawazboluwatife7 <147808555+Fawazboluwatife7@users.noreply.github.com>
2026-02-06 01:32:45 +0100

    updated the  two signatories17

--- server.js
+++ server.js
@@ -100,8 +100,12 @@ const imageOptions = {
   getImage: (tagValue) => {
     if (!tagValue) return null;
 
-    if (typeof tagValue !== "string") return null;
-  if (!tagValue.startsWith("data:image")) return null;
+//     if (typeof tagValue !== "string") return null;
+//   if (!tagValue.startsWith("data:image")) return null;
+
+if (typeof tagValue === "string" && tagValue.startsWith("{")) {
+    return null;
+  }
 
     const base64 = tagValue.replace(/^data:image\/\w+;base64,/, "");
     return Buffer.from(base64, "base64");
